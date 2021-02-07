1a42ef15f1825e7de152a2018bd115311af3ba1b
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-07 11:07:55 -0300

    Setting User Default an Remove other create users

--- src/modules/users/infra/typeorm/seeds/createUserSeed.ts
+++ src/modules/users/infra/typeorm/seeds/createUserSeed.ts
@@ -5,10 +5,9 @@ import User from '@modules/users/infra/typeorm/entities/User';
 class CreateUserSeed implements Seeder {
   public async run(factory: Factory): Promise<void> {
     await factory(User)().create({
-      name: 'Alcir Junior',
-      email: 'junior@cajucomunica.com.br'
+      email: 'barbeiro@gobarber.com.br'
     });
-    await factory(User)().createMany(50);
+    // await factory(User)().createMany(5);
   }
 }
 
